1c83de40f74dc2a61b6c28d9f7940e31bb1ec435
Leonardo Narciso <lnarcisoov@gmail.com>
2020-05-13 12:54:29 -0300

    Adding LIZARD and SPOCK to the shot class.

--- src/main/java/br/com/btg/constants/Shot.java
+++ src/main/java/br/com/btg/constants/Shot.java
@@ -4,4 +4,6 @@ public class Shot {
 	public static final int STONE = 1;
 	public static final int PAPER = 2;
 	public static final int SCISSOR = 3;
+	public static final int LIZARD = 4;
+	public static final int SPOCK = 5;
 }
