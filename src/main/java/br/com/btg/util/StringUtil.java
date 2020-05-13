8561d83b1d1b89b008ac8978ac8fad349143233f
Leonardo Narciso <lnarcisoov@gmail.com>
2020-05-13 15:35:56 -0300

    Adding StringUtil.

---
+++ src/main/java/br/com/btg/util/StringUtil.java
@@ -0,0 +1,19 @@
+package br.com.btg.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class StringUtil {
+
+	public static String objectToJSon(Object o) {
+		String json = "";
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			json = mapper.writeValueAsString(o);
+			System.out.println("ResultingJSONstring = " + json);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return json;
+	}
+}
