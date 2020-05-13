18ba71d56d83e8399a1d83fa1b403fec2842b103
Leonardo Narciso <lnarcisoov@gmail.com>
2020-05-13 10:42:07 -0300

    First commit.

---
+++ src/main/java/br/com/btg/JokenpoBtgApplication.java
@@ -0,0 +1,13 @@
+package br.com.btg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokenpoBtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokenpoBtgApplication.class, args);
+	}
+
+}
