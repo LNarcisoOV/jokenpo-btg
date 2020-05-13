d59d03b497039325922aeea40d0343335b722398
Leonardo Narciso <lnarcisoov@gmail.com>
2020-05-13 12:54:56 -0300

    Adding class for messages.

---
+++ src/main/java/br/com/btg/constants/Message.java
@@ -0,0 +1,7 @@
+package br.com.btg.constants;
+
+public class Message {
+	
+	public static final String PLAYER_ALREADY_EXISTS = "Couldn't create player because already exists player with this name.";
+
+}
