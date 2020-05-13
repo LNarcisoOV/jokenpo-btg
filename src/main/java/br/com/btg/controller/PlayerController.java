8c34f9361c3a9dc4b14634eb756e59f573d98f81
Leonardo Narciso <lnarcisoov@gmail.com>
2020-05-13 11:26:13 -0300

    Adding controller package and controller for players.

---
+++ src/main/java/br/com/btg/controller/PlayerController.java
@@ -0,0 +1,11 @@
+package br.com.btg.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/player")
+public class PlayerController {
+	
+
+}
