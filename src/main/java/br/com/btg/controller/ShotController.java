d0aa007f6448fc071df363d256b1d6a905af3cb7
Leonardo Narciso <lnarcisoov@gmail.com>
2020-05-14 10:43:40 -0300

    Adding classes and interface for shots.

---
+++ src/main/java/br/com/btg/controller/ShotController.java
@@ -0,0 +1,12 @@
+package br.com.btg.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  
+@RequestMapping("/shot")
+public class ShotController {
+	
+
+
+}
---
+++ src/main/java/br/com/btg/impl/ShotImpl.java
@@ -0,0 +1,8 @@
+package br.com.btg.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShotImpl implements ShotService{
+
+}
