f2cd3e5c2289993e9850d448daddca4c4b2007fb
Leonardo Narciso <lnarcisoov@gmail.com>
2020-05-15 11:58:49 -0300

    Correcting validation for input.

--- src/main/java/br/com/btg/constants/Message.java
+++ src/main/java/br/com/btg/constants/Message.java
@@ -2,7 +2,7 @@ package br.com.btg.constants;
 
 public class Message {
 	
-	public static final String INPUT_HAS_TO_BE_COMPLETE= "Couldn't create input because it has to be complete.";
+	public static final String INPUT_HAS_TO_BE_VALID= "Couldn't create input because input has to be valid.";
 	public static final String PLAYER_ALREADY_EXISTS = "Couldn't create player because already exists player with this name.";
 	public static final String INPUT_NOT_FOUND = "Couldn't find a input with this player name.";
 	public static final String SHOT_CANT_BE_EMPTY = "Shot can't be empty.";
--- src/main/java/br/com/btg/util/InputUtil.java
+++ src/main/java/br/com/btg/util/InputUtil.java
@@ -1,9 +1,17 @@
 package br.com.btg.util;
 
+import java.util.List;
+
+import br.com.btg.constants.Shot;
 import br.com.btg.model.Input;
 
 public class InputUtil {
+
+	static List<String> shotList = Shot.getAllKindOfShots();
+
 	public static boolean isAValidInput(Input input) {
-		return input != null && (!StringUtil.isNullOrEmpty(input.getPlayerName()) && !StringUtil.isNullOrEmpty(input.getShot()));
+		return input != null
+				&& (!StringUtil.isNullOrEmpty(input.getPlayerName()) && !StringUtil.isNullOrEmpty(input.getShot()))
+				&& shotList.contains(input.getShot());
 	}
 }
